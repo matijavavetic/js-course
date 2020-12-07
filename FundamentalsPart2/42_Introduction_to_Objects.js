c06a6bee3fe8c3555bf12b98d85fe1ee1764d8f9
Matija <matija.vavetic@gmail.com>
2020-12-07 13:28:36 +0100

    Lesson 42

---
+++ FundamentalsPart2/42_Introduction_to_Objects.js
@@ -0,0 +1,16 @@
+const jonasArray = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven']
+];
+
+// Object jonas, defined with {} brackets
+const jonas = {
+    firstName: 'Jonas',
+    lastname: 'Schmedtmann',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven']
+};
