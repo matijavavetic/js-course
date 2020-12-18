c887f2101bf3fa99e65eb1d7b9e5f12fa8fe177c
Matija <matija.vavetic@gmail.com>
2020-12-18 10:09:50 +0100

    Lesson 94

---
+++ Section8/94_Variable_Environment_Hoisting.js
@@ -0,0 +1,12 @@
+const myName = 'Jonas';
+
+if (myName === 'Jonas') {
+    // Temporal Dead Zone for job variable
+    console.log(`Jonas is a ${job}`);
+    const age = 2037 - 1989;
+    console.log(age);
+    // ------
+    
+    const job = 'teacher';
+    console.log(x);
+}
