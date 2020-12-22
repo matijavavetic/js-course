c5c67282afe66408c22a4d804beb78bae1c19e71
Matija <matija.vavetic@gmail.com>
2020-12-22 10:08:40 +0100

    Lesson 110

---
+++ Section9/110_Looping_arrays_for-of.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  
+    openingHours: {
+      thu: {
+        open: 12,
+        close: 22,
+      },
+      fri: {
+        open: 11,
+        close: 23,
+      },
+      sat: {
+        open: 0, // Open 24 hours
+        close: 24,
+      },
+  
+      order: function(starterIndex, mainIndex) {
+          return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+      },
+  
+      orderDelivery: function({starterIndex = 1, mainIndex = 0, time = '20:00', address}) {
+          console.log(`Order received! ${this.starterMenu[starterIndex]} and
+          ${this.mainMenu[mainIndex]}, will be delivered to ${address} at ${time}`);
+      }
+    },
+};
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for (const item of menu) {
+    console.log(item);
+}
+
+// To get current iteration as array
+for (const item of menu.entires()) {
+    console.log(`${item[0] + 1}: ${item[1]}`);
+}
+
+console.log([...menu.entries()]);
