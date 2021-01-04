fd0ad17c7b2127936afd9cd83bfdefd4a86a630c
Matija Vavetic <matija.vavetic@gmail.com>
2021-01-04 09:47:02 +0100

    Lesson 131

---
+++ src/Section10/131_The_call_and_apply_methods.js
@@ -0,0 +1,42 @@
+const lufthansa = {
+    airline: 'Lufthansa',
+    iataCode: 'LH',
+    bookings: [],
+    book(flightNum, name) {
+        console.log(`
+        ${name} booked a seat on ${this.airline}
+        flight ${this.iataCode}${flightNum}`);
+        this.bookings.push({flight: `${this.iataCode}${flightNum}`, name});
+    },
+};
+
+lufthansa.book(239, 'Jonas');
+lufthansa.book(635, 'John');
+
+const eurowings = {
+    airline: 'Eurowings',
+    iataCode: 'EW',
+    bookings: []
+};
+
+const book = lufthansa.book;
+
+// Does not work
+// book(23, 'Sarah');
+
+// Works, eurowings parameter explicity tells JS
+// that 'this' keyword is eurowings object
+book.call(eurowings, 23, 'Sarah');
+console.log(eurowings);
+
+book.call(lufthansa, 239, 'Mary');
+console.log(lufthansa);
+
+const swiss = {
+    airline: 'Swiss',
+    iataCode: 'SW',
+    bookings: []
+};
+
+book.call(swiss, 212, 'MAry');
+console.log(swiss);
